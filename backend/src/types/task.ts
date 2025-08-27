d4ad7f2a6cc736e252e4f2571a28c0eeb2607bfd
Olaf1303 <olaf.koziara@gmail.com>
2025-08-27 17:40:38 +0200

    move task form scheme to client

--- backend/src/types/task.ts
+++ backend/src/types/task.ts
@@ -25,11 +25,6 @@ export const taskBaseSchema = z.object({
   tags: z.array(z.string()).default([]),
 });
 
-export const taskFormSchema = taskBaseSchema.extend({
-  tags: z.string().transform((val) => 
-    val ? val.split(',').map((tag) => tag.trim()) : []
-  ).optional(),
-});
 
 export const createTaskSchema = taskBaseSchema.extend({
   createdBy: z.string(),
@@ -46,7 +41,6 @@ export const taskQuerySchema = z.object({
 });
 
 export type Task= z.infer<typeof taskBaseSchema>;
-export type TaskFormValues = z.infer<typeof taskFormSchema>;
 export type CreateTaskInput = z.infer<typeof createTaskSchema>;
 export type UpdateTaskInput = z.infer<typeof updateTaskSchema>;
 export type TaskQueryInput = z.infer<typeof taskQuerySchema>;
--- frontend/src/features/tasks/types/index.ts
+++ frontend/src/features/tasks/types/index.ts
@@ -1,4 +1,16 @@
-import { RouterOutput } from "@/types";
+import { RouterInput, RouterOutput } from "@/types";
 import { TaskStatus,Priority } from "../../../../../backend/src/types/task";
-export type Task = RouterOutput["tasks"]["get"];
+import z from "zod";
+export type Task = RouterInput["tasks"]["create"];
 export { TaskStatus, Priority };
+export const taskFormSchema = z.object({
+  title: z.string().min(1, 'Title is required').max(100),
+  description: z.string().max(500),
+  status: z.nativeEnum(TaskStatus).default(TaskStatus.TODO),
+  priority: z.nativeEnum(Priority).default(Priority.MEDIUM),
+  dueDate: z.date().optional(),
+  tags: z.string().transform((val) => 
+    val ? val.split(',').map((tag) => tag.trim()) : []
+  ).optional(),
+});
+export type TaskFormValues = z.infer<typeof taskFormSchema>;
