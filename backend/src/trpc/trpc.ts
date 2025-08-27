c208c26e273bd44d00349bfa59941823bff0caf6
Olaf1303 <olaf.koziara@gmail.com>
2025-08-27 09:37:23 +0200

    remove unused import

--- backend/src/trpc/trpc.ts
+++ backend/src/trpc/trpc.ts
@@ -1,7 +1,6 @@
 import { initTRPC, TRPCError } from '@trpc/server';
 import { CreateExpressContextOptions } from '@trpc/server/adapters/express';
 import jwt from 'jsonwebtoken';
-import { User } from '../models/user.model';
 
 // Create context type
 export interface Context {
