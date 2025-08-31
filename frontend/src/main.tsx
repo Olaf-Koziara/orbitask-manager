70da967d284d3c7faa6fccd16d6e5f9bc166b0d0
Olaf1303 <olaf.koziara@gmail.com>
2025-08-31 15:03:44 +0200

    remove query client duplicate

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { trpc, trpcClient } from './utils/trpc';
+import {  QueryClientProvider } from '@tanstack/react-query';
+import { queryClient, trpc, trpcClient } from './utils/trpc';
 import App from './App';
 import './index.css';
 
-const queryClient = new QueryClient();
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
