e4b2083d6e24a1c4729d07f9c000b287b14b1fcb
Olaf1303 <olaf.koziara@gmail.com>
2025-09-23 18:18:20 +0200

    refactor: clean up main.tsx by removing tRPC providers

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {  QueryClientProvider } from '@tanstack/react-query';
-import { queryClient, trpc, trpcClient } from './api/trpc';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </trpc.Provider>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
