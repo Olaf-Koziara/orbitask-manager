fd9dae2f9a71a7c3ea8c3be2d432bed0333de99d
Olaf1303 <olaf.koziara@gmail.com>
2026-01-01 21:10:29 +0100

    Update frontend/src/features/projects/schemas/project.schema.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- frontend/src/features/projects/schemas/project.schema.ts
+++ frontend/src/features/projects/schemas/project.schema.ts
@@ -3,6 +3,6 @@ import z from "zod";
 export const projectFormSchema = z.object({
   name: z.string().trim().nonempty("Project name is required").min(2, "Name must be at least 2 characters").max(50, 'Name must be 50 characters or less'),
   description: z.string().max(500, "Description must be 500 characters or less").optional(),
-  color: z.string().min(7, "Please select a color").max(7).regex(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/, 'Invalid color format'),
+  color: z.string().min(7, "Please select a color").max(7).regex(/^#[A-Fa-f0-9]{6}$/, 'Invalid color format'),
   participants: z.array(z.string()).optional().default([]),
 });
